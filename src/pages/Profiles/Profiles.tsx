a1283342859a36cc329f021f4ed2e6157e710a33
CCMatson <catherine.matson@gmail.com>
2023-02-24 14:07:14 -0800

    display profiles

--- src/pages/Profiles/Profiles.tsx
+++ src/pages/Profiles/Profiles.tsx
@@ -1,37 +1,31 @@
-// npm packages
-import { useState, useEffect } from 'react'
-
-// services
-import * as profileService from '../../services/profileService'
-
 // types
 import { Profile } from '../../types/models'
 
-const Profiles = (): JSX.Element => {
-  const [profiles, setProfiles] = useState<Profile[]>([])
 
-  useEffect((): void => {
-    const fetchProfiles = async (): Promise<void> => {
-      try {
-        const profileData: Profile[] = await profileService.getAllProfiles()
-        setProfiles(profileData)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchProfiles()
-  }, [])
+// // components
+// import ProfileCard from '../../components/ProfileCard/ProfileCard'
+
+interface ProfilesProps {
+  profiles: Profile[];
+  // handleVote: (formData: VoteManagerFormData) => void;
+}
+
+const Profiles = (props: ProfilesProps): JSX.Element => {
+  const { profiles } = props
 
   if(!profiles.length) return <p>No profiles yet</p>
 
   return (
-    <>
-      <h1>Hello. This is a list of all the profiles.</h1>
+    <main className='list'>
+      <h1>Hello, here is the list of  profiles</h1>
       {profiles.map((profile: Profile) =>
-        <p key={profile.id}>{profile.name}</p>
+      <p key={profile.id}>{profile.name}</p>
+
+        // <ProfileCard profile={profile} key={profile.id} handleVote={props.handleVote} />
       )}
-    </>
+    </main>
   )
 }
- 
+
 export default Profiles
+
