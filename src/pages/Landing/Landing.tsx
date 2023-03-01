3e60de919b141b59747fd299ccfb3e9ac6eaa99b
CCMatson <catherine.matson@gmail.com>
2023-03-01 00:16:57 -0800

    fix landing page text

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -15,7 +15,7 @@ const Landing = (props: LandingProps): JSX.Element => {
     <main className={styles.container}>
       <h1>Hello, {user ? user.name : 'shoe collector'}</h1>
       <h1>Welcome to the Shoe Collection</h1>
-      <h2>Click the links above to visit the shoe gallery or to add new shoes.</h2>
+      <h2>Click the links above to explore the app.</h2>
       <div className={styles.icons}>
         <div><img src="https://i.imgur.com/uSgCzyu.png" alt="" /></div>
         <div><img src="https://i.imgur.com/D4hhViw.png" alt="" /></div>
